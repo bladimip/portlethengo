87a3cb7285c92847073cd20bc4d44d7cba4f2ae6
duntik <duntiknews@yandex.ru>
2016-10-03 15:33:12 +0100

    jhgj

--- index.php
+++ index.php
@@ -3,6 +3,16 @@
       <title>---</title>
      </head>
  <body>
- <?php echo '<p>Wow</p>'; ?>
+ <?php 
+$start = 20;
+$end = 45;
+$sum = 0;
+
+for($i = $start; $i <= $end; $i++) {
+    if(fmod($i, 5) == 0) {
+        $sum += $i;
+    }
+}
+echo $sum; ?>
  </body>
-</html> 
+</html>
