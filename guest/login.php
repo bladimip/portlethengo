788b407ea4baea0a09261153107e6653c66d471d
duntik <duntiknews@yandex.ru>
2016-10-10 15:20:54 +0100

    uu

---
+++ guest/login.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1407563
+ * Date: 10/10/2016
+ * Time: 15:19
+ */
+
+echo 'Login';
--- index.php
+++ index.php
@@ -5,7 +5,14 @@ else {
    $page = substr($_SERVER['REQUEST_URI'], 1);
     if( !preg_match('/^[A-z0-9]{3,15}$/', $page) ) exit('error url');
 }
-echo $page;
+
+session_start();
+
+if( file_exists('all/'.$page.'.php')) include 'all/'.$page.'.php';
+else if( $_SESSION['uLogin'] = 1 and file_exists('auth/'.$page.'.php')) include 'auth/'.$page.'.php';
+else if( $_SESSION['uLogin'] != 1 and file_exists('guest/'.$page.'.php')) include 'guest/'.$page.'.php';
+else exit('Page404')
+
 ?>
 
 <!DOCTYPE html>
