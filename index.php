7c5c2df3c69ce002c99983a01053de360c296baa
duntik <duntiknews@yandex.ru>
2016-10-10 14:25:31 +0100

    uu

--- index.php
+++ index.php
@@ -3,24 +3,3 @@
 ver_dump($_SERVER);
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-<?php
-$start = 20;
-$end = 90;
-$sum = 0;
-for($i = $start; $i <= $end; $i++) {
-    if(fmod($i, 5) == 0) {
-        $sum += $i;
-    }
-}
-echo $sum;
-?>
-</body>
-</html>
