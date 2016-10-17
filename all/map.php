0de1e5995e6162b5f4b32bac4c757db79f22ee76
duntik <duntiknews@yandex.ru>
2016-10-17 16:32:46 +0100

    uu

--- all/map.php
+++ all/map.php
@@ -5,6 +5,9 @@ echo "Hello World";
 ?>
 
 <script>
+    <div id="map"></div>
+        <script>
+
     var map;
     function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
@@ -12,6 +15,10 @@ echo "Hello World";
             zoom: 8
         });
     }
+
+</script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=r6H8oL-lra0lgTmkg6d7pR5Assg=&callback=initMap"
+            async defer></script>
 </script>
 
 <? bottom() ?>
