f4ab82c27c535f41401dc26d02ca57f2c07d3c50
duntik <duntiknews@yandex.ru>
2016-10-17 14:10:26 +0100

    uu

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ echo '<!DOCTYPE html>
 <head>
 <meta charset="UTF-8">
 <title>'.$title.'</title>
-<link type="css" rel="stylesheet" href="/style.css">
+<link type="css/text" rel="stylesheet" href="/style.css">
 <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 <script src="https://code.jquery.com/jquery-1.12.4.js" integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU=" crossorigin="anonymous"></script>
 <script src="/script.js"></script>
@@ -69,7 +69,7 @@ echo '<!DOCTYPE html>
 
 <div class="menu">
 <a href="/">Home</a>
-<a href="/login">login in</a>
+<a href="/login">Login in</a>
 <a href="/register">Registration</a>
 </div>
 
