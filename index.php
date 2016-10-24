842c2905a3e6f6889fdbc7878e22157e6f4bcec4
duntik <duntiknews@yandex.ru>
2016-10-24 15:30:39 +0100

    uu

--- index.php
+++ index.php
@@ -82,7 +82,7 @@ echo '<!DOCTYPE html>
 <link  rel="stylesheet" href="style.css">
 <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 <script src="https://code.jquery.com/jquery-1.12.4.js" integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU=" crossorigin="anonymous"></script>
-<script src="/fordb/script.js"></script>
+<script src="/script.js"></script>
 <script src="https://maps.googleapis.com/maps/api/js?key=r6H8oL-lra0lgTmkg6d7pR5Assg=&callback=initMap" async defer></script>
 </head>
 
