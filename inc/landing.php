9aa1ea1f66df3b4b3ebd519e8a72a0acd138a11d
duntik <duntiknews@yandex.ru>
2016-11-07 17:42:19 +0000

    new footer

--- inc/landing.php
+++ inc/landing.php
@@ -10,15 +10,42 @@ top("Welcome to Portlethen");
 
 //Other page content
 ?>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ullamcorper mauris non risus egestas consectetur. Ut non lacus lorem. Cras sagittis porta neque eu egestas. In id porttitor nunc. Morbi ultrices sagittis dolor. Proin viverra in lectus vel pharetra. Suspendisse convallis mauris et nulla condimentum dictum. Duis et rhoncus nulla, in sagittis leo. Cras congue sodales leo in volutpat. Curabitur euismod felis dapibus nisi porta lobortis. In semper, mi vel blandit elementum, justo nisl imperdiet justo, eget tempus ligula turpis non risus.
+    <div class="parallax-container valign-wrapper">
+        <div class="section no-pad-bot">
+            <div class="container">
+                <div class="row center">
+                    <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+                </div>
+            </div>
+        </div>
+        <div class="parallax"><img src="background2.jpg" alt="Unsplashed background img 2"></div>
+    </div>
 
-Sed orci felis, tincidunt in turpis a, viverra faucibus arcu. Vivamus tempus nisi ante, auctor varius lectus volutpat ac. Vivamus vehicula facilisis felis. Nam auctor vehicula ultricies. Aliquam erat volutpat. Maecenas quis ligula eu odio tincidunt ullamcorper id a sapien. Fusce quam felis, vestibulum ac commodo pulvinar, tincidunt et lorem. Ut laoreet vitae mi eget pretium.
+    <div class="container">
+        <div class="section">
 
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus imperdiet leo quis sem dignissim, vestibulum rhoncus tellus pharetra. Nam sem turpis, venenatis in rutrum et, vulputate ut tellus. Nunc semper dignissim porttitor. Integer iaculis nec quam a fringilla. Donec fermentum id magna eget porta. Morbi vel ultrices lacus. Phasellus tincidunt venenatis consectetur. Quisque sollicitudin justo non sapien efficitur, non ultricies eros elementum. Phasellus euismod lacus vitae est congue, consectetur semper eros ultricies. Morbi ac elementum augue. Morbi quis porttitor tellus.
+            <div class="row">
+                <div class="col s12 center">
+                    <h3><i class="mdi-content-send brown-text"></i></h3>
+                    <h4>Contact Us</h4>
+                    <p class="left-align light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+                </div>
+            </div>
 
-Vivamus eget ligula sed risus vehicula sagittis non ut urna. Sed ac condimentum mauris. Nam in elementum metus. Pellentesque vestibulum, metus et finibus pretium, ligula lorem viverra dui, sit amet ultrices turpis enim et mi. Maecenas laoreet commodo lobortis. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras condimentum et enim at aliquam. Mauris laoreet placerat orci eget porta. Vestibulum nec iaculis metus, ut sollicitudin est. Aliquam erat volutpat. Fusce ipsum neque, pharetra non volutpat a, fringilla ut purus. Sed a erat lacus.
+        </div>
+    </div>
 
-Donec consequat enim augue, porttitor venenatis augue dignissim sit amet. Quisque tempus facilisis mattis. Cras vitae condimentum sem. Vestibulum at justo massa. Vivamus ultrices sollicitudin vehicula. Curabitur molestie ac est quis efficitur. Donec tempor justo eros, ac hendrerit quam dictum lacinia. Duis in nulla tempor, iaculis est ac, porta leo. Suspendisse potenti. Vestibulum non vulputate ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+    <div class="parallax-container valign-wrapper">
+        <div class="section no-pad-bot">
+            <div class="container">
+                <div class="row center">
+                    <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+                </div>
+            </div>
+        </div>
+        <div class="parallax"><img src="background3.jpg" alt="Unsplashed background img 3"></div>
+    </div>
 <?php
 
 // Footer
