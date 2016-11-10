714ea6c961bef8ed32e79587eb57f8e289db8463
Arnuga3 <20136310@nescol.ac.uk>
2016-11-10 22:59:17 +0000

    some changes

--- inc/sportlethen.php
+++ inc/sportlethen.php
@@ -46,7 +46,7 @@ if (isset($_GET["clubs_genre"])) {
     echo '<div class="row">';
     while ($row = $clubs->fetch_assoc()) {
             echo '
-            <a href="/sportlethen/club/'. urlencode($row["club_id"]) .'">
+            <a href="/sportlethen/'. urlencode($row["category"]) .'/'. urlencode($row["name"]) .'">
                 <div class="sp-genre-list z-depth-1 col s12">
                     '. $row["name"] .'
                 </div>
@@ -61,12 +61,14 @@ if (isset($_GET["clubs_genre"])) {
     $clubs = $db->runQuery("SELECT category, code, genreCode, COUNT(*) AS num FROM clubgenre, clubs WHERE genreCode = code GROUP BY code");
     $db->close();
 
+    echo '<h4 class="sp-title">Discover Portlethen Clubs</h4>';
+
     echo '<div class="row">';
     while ($row = $clubs->fetch_assoc()) {
             echo '
-            <a href="/sportlethen/genre/'. urlencode($row["category"]) .'">
-                <div class="sp-genre z-depth-2 col s12 m6 l3">
-                    '. $row["category"] .' ('. $row["num"] .')
+            <a href="/sportlethen/'. urlencode($row["category"]) .'">
+                <div class="col s12 m6 l3">
+                    <p class="sp-genre z-depth-2">'. $row["category"] .' <br>('. $row["num"] .')</p>
                 </div>
             </a>';
     }
