7c0e1eec7629bd77528f1292b0aaa28922146d42
Arnuga3 <20136310@nescol.ac.uk>
2016-11-10 02:11:50 +0000

    club genre page functionality complete

--- inc/sportlethen.php
+++ inc/sportlethen.php
@@ -34,13 +34,43 @@ include('layouts/HTMLcomponents.php');
 // Navbar
 top("Clubs and Societies");
 
-//Other page content
-$db = new Connection();
-$db->open();
-$clubs = $db->runQuery("SELECT * FROM clubs");
-$db->close();
+if (isset($_GET["clubs_genre"])) {
+    $clGenre = $_GET["clubs_genre"];
+    $clGenreDecoded = urldecode($clGenre);
 
-while ($row = $clubs->fetch_assoc()) {
+    $db = new Connection();
+    $db->open();
+    $clubs = $db->runQuery("SELECT * FROM clubs,clubgenre WHERE genreCode = code AND category = '". $clGenreDecoded."'");
+    $db->close();
+
+    echo '<div class="row">';
+    while ($row = $clubs->fetch_assoc()) {
+            echo '
+            <a href="/sportlethen/club/'. urlencode($row["club_id"]) .'">
+                <div class="sp-genre-list z-depth-1 col s12">
+                    '. $row["name"] .'
+                </div>
+            </a>';
+    }
+    echo '</div>';
+
+} else {
+
+    $db = new Connection();
+    $db->open();
+    $clubs = $db->runQuery("SELECT category, code, genreCode, COUNT(*) AS num FROM clubgenre, clubs WHERE genreCode = code GROUP BY code");
+    $db->close();
+
+    echo '<div class="row">';
+    while ($row = $clubs->fetch_assoc()) {
+            echo '
+            <a href="/sportlethen/genre/'. urlencode($row["category"]) .'">
+                <div class="sp-genre z-depth-2 col s12 m6 l3">
+                    '. $row["category"] .' ('. $row["num"] .')
+                </div>
+            </a>';
+    }
+    echo '</div>';
 
 }
 
