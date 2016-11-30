37d571948e8e1d03b7ffd7f79cd148000ee468f3
Vladimir R <bladimip@gmail.com>
2016-11-30 17:56:10 +0000

    continuing profile
    getting information displayed and it's layout, formatting will be left
    for later

--- auth/t_profile.php
+++ auth/t_profile.php
@@ -19,20 +19,25 @@ include('inc/layouts/HTMLcomponents.php');
 //file contains information and methods for DB
 include('inc/db/simpleDB.php');
 
+//method for fetching queries - returns query result
+//used methods described in simpleDB file
+function getContributions ($table, $userID) {
+	$db = new Connection();
+	$db->open();
+	$queryResult = $db->runQuery("SELECT * FROM " . $table . " WHERE user_id = ". $userID);
+	$db->close();
+	return $queryResult;
+}
 
 // Navbar
 top("vladTest");
 
 //Other page content
 //setting testing variable - browsed user's id:
-$person = 5;
+$person = 9;
 
-//running methods for running sql query and getting user data
-//methods described in simpleDB file
-$db = new Connection();
-$db->open();
-$user = $db->runQuery("SELECT * FROM Users WHERE user_id = '". $person ."' LIMIT 1");
-$db->close();
+//running method for getting sql query with user data - used for getting data from one table
+$user = getContributions("Users", $person);
 
 //writing queried user data into variables
 //not all data will be used in final version
@@ -43,67 +48,71 @@ while ($row = $user->fetch_assoc()) {
 	$siteAdmin = $row["siteAdmin"];
     $userName = $row["username"];
 	$email = $row["email"];
-	$password = $row["password"];
+	//$password = $row["password"];
 	$blocked = $row["blocked"];
 }
 //outputting all info - for testing purposes
-echo $userID;
-echo $clubAdmin;
-echo $nkpag;
-echo $siteAdmin;
-echo $userName;
-echo $email;
-echo $password;
-echo $blocked;
+// echo "<h1>" . $userName . "</h1>";
+// echo "<h2>" . $userName . "</h2>";
+// echo "<h3>" . $userName . "</h3>";
+// echo "<h4>" . $userName . "</h4>";
+// echo "<h5>" . $userName . "</h5>";
 
-//getting administrated clubs
-$db = new Connection();
-$db->open();
-$clubs = $db->runQuery("SELECT * FROM ClubAdmins WHERE user_id = ". $person);
-$db->close();
+echo "<h2>" . $userName . "</h2>";
 
-// while ($row = $clubs->fetch_assoc()) {
-	// echo = $row["club_id"];
-// }
+if ($blocked == 1) {
+	echo "This account is disabled<br>";
+}
+
+if ($siteAdmin == 1) {
+	echo "Site Administrator<br>";
+} else if ($nkpag == 1) {
+	echo "Map Admin<br>";
+}
+
+echo "User ID: " . $userID . "<br>";
+echo "Registered email address: " . $email . "<br>";
+
+
+if ($clubAdmin == 1) {
+	echo ("Admin of following clubs: ");
+	//getting administrated clubs
+	$clubs = getContributions("ClubAdmins", $person);
+	while ($row = $clubs->fetch_assoc()) {
+		echo $row["club_id"];
+	}
+}
 
 //getting added events
-$db = new Connection();
-$db->open();
-$events = $db->runQuery("SELECT * FROM ClubEvents WHERE user_id = ". $person);
-$db->close();
-
-// while ($row = $events->fetch_assoc()) {
-	// echo = $row["event_id"];
-// }
+$events = getContributions("ClubEvents", $person);
+echo "<br>Added events:<br>";
+while ($row = $events->fetch_assoc()) {
+	echo $row["name"] . "<br>";
+}
 
 //getting added articles
-$db = new Connection();
-$db->open();
-$news = $db->runQuery("SELECT * FROM HealthNews WHERE user_id = ". $person);
-$db->close();
-
-// while ($row = $news->fetch_assoc()) {
-	// echo = $row["news_id"];
-// }
+$news = getContributions("HealthNews", $person);
+echo "<br>Added articles:<br>";
+while ($row = $news->fetch_assoc()) {
+	echo $row["title"] . "<br>";
+}
 
 //getting added locations
-$db = new Connection();
-$db->open();
-$locations = $db->runQuery("SELECT * FROM Locations WHERE user_id = ". $person);
-$db->close();
-
-// while ($row = $locations->fetch_assoc()) {
-	// echo = $row["loc_id"];
-// }
+$locations = getContributions("Locations", $person);
+echo "<br>Added locations:<br>";
+while ($row = $locations->fetch_assoc()) {
+	echo $row["name"] . "<br>";
+}
 
 //getting added routes
-$db = new Connection();
-$db->open();
-$routes = $db->runQuery("SELECT * FROM Routes WHERE user_id = ". $person);
-$db->close();
+$routes = getContributions("Routes", $person);
+echo "<br>Added routes:<br>";
+while ($row = $routes->fetch_assoc()) {
+	echo $row["name"] . "<br>";
+}
 
-// while ($row = $routes->fetch_assoc()) {
-	// echo = $row["route_id"];
+// if (mysql_num_rows($routes)== 0) {
+	// echo "OK";
 // }
 
 // Footer
