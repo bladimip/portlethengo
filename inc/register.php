257558d756e0474a589478488d0477d7792415c9
arnuga3 <arniszelcs@gmail.com>
2016-12-05 14:19:40 +0000

    changes

--- inc/register.php
+++ inc/register.php
@@ -1,12 +1,12 @@
 <?php
-/* 
+/*
 Registration
 */
 
 include('db/simpleDB.php');
 include('layouts/HTMLcomponents.php');
 Ulogin(0);
-error_reporting(0);
+//error_reporting(0);
 
 if ($_POST['enter']) {
     $_POST['login'] = FormChars($_POST['login']);
@@ -14,15 +14,18 @@ if ($_POST['enter']) {
     $_POST['password'] = GenPass(FormChars($_POST['password']), $_POST['login']);
 
     if (!$_POST['login'] or !$_POST['email'] or !$_POST['password']) exit('DATA ENTRED ERROR');
-    
+
     if ($CONNECT){
     $Row = (mysqli_query($CONNECT, "SELECT `login` FROM `users` WHERE `login` = '$_POST[login]'"));
     if ($Row['login']) exit('Login <b>'.$_POST['login'].'</b> allready in use');
     $Row = mysqli_fetch_assoc(mysqli_query($CONNECT, "SELECT `email` FROM `users` WHERE `email` = '$_POST[email]'"));
     if ($Row['email']) exit('Email <b>'.$_POST['email'].'</b> allready in use');
 }
-    mysqli_query($CONNECT, "INSERT INTO `users`  VALUES ('', '0', '0', '0', '$_POST[login]', '$_POST[email]', '$_POST[password]', '0')");
-
+    //mysqli_query($CONNECT, "INSERT INTO `users`  VALUES ('', '0', '0', '0', '$_POST[login]', '$_POST[email]', '$_POST[password]', '0')");
+    $db = new Connection();
+    $db->open();
+    $db->runQuery("INSERT INTO `users` (clubAdmin, nkpag, siteAdmin, username, email, password, blocked)  VALUES ('0', '0', '0', '$_POST[login]', '$_POST[email]', '$_POST[password]', '0')");
+    $db->close();
     header ('Location: login');
     }
 
