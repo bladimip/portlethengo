162aae7b8b976271445a33cf72e059f70c1e5306
Ilja <duntiknews@yandex.ru>
2016-12-07 12:19:15 +0000

    Update setting.php

--- setting.php
+++ setting.php
@@ -1,8 +1,8 @@
 <?php
 
-define ('HOST', 'localhost');
-define ('USER', 'root');
-define ('PASS', '');
+define ('HOST', 'us-cdbr-azure-southcentral-f.cloudapp.net');
+define ('USER', 'b04a42ad414e01');
+define ('PASS', '686a923d');
 define ('DB', 'webdev5');
 
 ?>
