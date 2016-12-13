83fecca71cda3395e5cd49f55ebadcdfdf2b9352
scott <scotsinclair@gmail.com>
2016-12-13 17:34:45 +0000

    Changes to getUserID function now should work with $_SESSION

--- inc/map/addmap.php
+++ inc/map/addmap.php
@@ -19,10 +19,16 @@ include('../layouts/HTMLcomponents.php');
 // Navbar
 top("Locations and Routes");
 
-//TODO placeholder function to return the user_ID from session, to be rewritten
 function getUserID(){
-    return ;
-}
+    //needs to check for logged in user and return user_id, return 0 for no active login
+    if (isset($_SESSION['USER_LOGIN_IN'])) {
+        $user_id = $_SESSION['USER_ID'];
+        return $user_id;
+    }
+    else{
+        return 0;
+    }
+};
 //Other page content
 
 
