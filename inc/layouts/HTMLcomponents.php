1dd45686bce786d9b8ff071cb14fda8ecaf3a9fc
arnuga3 <arniszelcs@gmail.com>
2016-12-13 15:35:08 +0000

    some more changes

--- inc/layouts/HTMLcomponents.php
+++ inc/layouts/HTMLcomponents.php
@@ -73,10 +73,10 @@ function top( $title ) {
 						<a href="/" class="left brand-logo lime-text"><span class="lnr lnr-apartment"></span>Go-Portlethen</a>
 
 						<ul class="right hide-on-med-and-down">
-							<li><a class="waves-effect waves-light" href="/search"><span class="lnr lnr-magnifier"></span>Search</a></li>
-							<li><a class="waves-effect waves-light" href="/sportlethen"><span class="lnr lnr-users"></span>SPortlethen</a></li>
-							<li><a class="waves-effect waves-light" href="/health-wellbeing"><span class="lnr lnr-heart-pulse"></span>Health & Wellbeing</a></li>
-							<li><a class="waves-effect waves-light" href="/map"><span class="lnr lnr-map"></span>Discover Area</a></li>
+							<li><a class="waves-effect waves-light" href="search.php"><span class="lnr lnr-magnifier"></span>Search</a></li>
+							<li><a class="waves-effect waves-light" href="sportlethen.php"><span class="lnr lnr-users"></span>SPortlethen</a></li>
+							<li><a class="waves-effect waves-light" href="health-wellbeing.php"><span class="lnr lnr-heart-pulse"></span>Health & Wellbeing</a></li>
+							<li><a class="waves-effect waves-light" href="map.php"><span class="lnr lnr-map"></span>Discover Area</a></li>
 							<li><a class="waves-effect waves-light logBtn"  href="#!"><span class="lnr lnr-user"></span>
 								<?php if (isset($_SESSION['USER_LOGIN_IN'])) echo '<span>Logout</span>';
 											else echo '<span>Login</span>'; ?>
