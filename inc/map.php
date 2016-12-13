e8e871565e3efee0067dcfdfff6627f46f1f46e6
scott <scotsinclair@gmail.com>
2016-12-13 13:56:52 +0000

    test

--- inc/map.php
+++ inc/map.php
@@ -1,7 +1,7 @@
 <?php
 
-//header('location: /map/map.php');
-echo "the routing is broken, this is the wrong file this is /inc/map.php, ";
-echo "you are looking for /inc/map/map.php";
+header('location: /map/map.php');
+//echo "the routing is broken, this is the wrong file this is /inc/map.php, ";
+//echo "you are looking for /inc/map/map.php";
 
 ?>
